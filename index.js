b58ec839c2704a1d82bd056d897e3a73b563553b
HDegroote <75906619+HDegroote@users.noreply.github.com>
2025-02-04 10:21:59 +0100

    Add errors

--- index.js
+++ index.js
@@ -12,9 +12,38 @@ const postEncoding = {
   responseEncoding: c.none
 }
 
+class BlindPeerError extends Error {
+  constructor (msg, code, fn = BlindPeerError) {
+    super(`${code}: ${msg}`)
+    this.code = code
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, fn)
+    }
+  }
+
+  get name () {
+    return 'BlindPeerError'
+  }
+
+  static MAILBOX_NOT_FOUND () {
+    return new BlindPeerError('Mailbox not found', 'MAILBOX_NOT_FOUND', BlindPeerError.MAILBOX_NOT_FOUND)
+  }
+
+  static fromRpcCause (cause) {
+    switch (cause.code) {
+      case 'MAILBOX_NOT_FOUND':
+        return BlindPeerError.MAILBOX_NOT_FOUND(cause.message)
+      default:
+        throw new BlindPeerError(cause.message)
+    }
+  }
+}
+
 module.exports = {
   definition,
   schema,
   addMailboxEncoding,
-  postEncoding
+  postEncoding,
+  BlindPeerError
 }
