58b3bd102f7306f8f866214c64643960394b1c32
HDegroote <75906619+HDegroote@users.noreply.github.com>
2025-02-12 13:41:00 +0100

    Add errors (#1)
    * Add errors
    
    * Add invalid-mailbox id

--- index.js
+++ index.js
@@ -12,9 +12,44 @@ const postEncoding = {
   responseEncoding: c.none
 }
 
+class BlindPeerError extends Error {
+  constructor (msg, code, fn = BlindPeerError) {
+    super(`${code}: ${msg}`)
+    this.code = code
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, fn)
+    }
+  }
+
+  get name () {
+    return 'BlindPeerError'
+  }
+
+  static MAILBOX_NOT_FOUND () {
+    return new BlindPeerError('Mailbox not found', 'MAILBOX_NOT_FOUND', BlindPeerError.MAILBOX_NOT_FOUND)
+  }
+
+  static INVALID_MAILBOX_ID () {
+    return new BlindPeerError('Invalid mailbox id', 'INVALID_MAILBOX_ID', BlindPeerError.INVALID_MAILBOX_ID)
+  }
+
+  static fromRpcCause (cause) {
+    switch (cause.code) {
+      case 'MAILBOX_NOT_FOUND':
+        return BlindPeerError.MAILBOX_NOT_FOUND(cause.message)
+      case 'INVALID_MAILBOX_ID':
+        return BlindPeerError.INVALID_MAILBOX_ID(cause.message)
+      default:
+        throw new BlindPeerError(cause.message)
+    }
+  }
+}
+
 module.exports = {
   definition,
   schema,
   addMailboxEncoding,
-  postEncoding
+  postEncoding,
+  BlindPeerError
 }
